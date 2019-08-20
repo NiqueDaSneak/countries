f9bc23dda624c8e9e6b814e61d0a09875fba648c
Dom Clemmer <domclemmer@Doms-MacBook-Air.local>
2019-08-20 16:51:04 -0400

    implement router as base component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Countries from './pages/Countries'
+import Country from './pages/Country'
+
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const AppRouter = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path='/' component={Home} />
+      <Route path='/countries' component={Countries} />
+      <Route path='/country/:code' component={Home} />
+    </Router>
   );
 }
 
-export default App;
+export default AppRouter;
